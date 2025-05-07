0d1bbc063afb8fe54b791d679f381774107bb511
JoaoGHIT-creator <joaooalves74@gmail.com>
2025-05-07 19:34:27 -0300

    selectos list added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,20 +1,30 @@
 describe('Orange HRM tests', () => {
-  
+const selectorsList = {
+usernameField: "[name='nickname']",
+professionalField: "[name='username']",
+passwordField: "[name='password']",
+loginButton: "[type='submit']",
+sectionTitletopbar: ".oxd-topbar-header-breadcrumb-module",
+wrongCredentialAlert: "[role='alert']",
+};
+
   it('login - success', () => {  // Corrigido 'sucess' para 'success'
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get(elementsList.professionalField).should('be.visible').type('Admin');
-    cy.get("[name='password']").should('be.visible').type('admin123');
-    cy.get("[type='submit']").should('be.visible').click();
+    cy.get(selectorsList.professionalField).should('be.visible').type('Admin'); // Corrigido referência para selectorsList
+    cy.get(selectorsList.passwordField).should('be.visible').type('admin123');
+    cy.get(selectorsList.loginButton).should('be.visible').click();
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index');
-    cy.get(".oxd-topbar-header-breadcrumb-module").should('contain', 'Dashboard'); // Corrigido seletor com "." antes da classe
+    cy.get(selectorsList.sectionTitletopbar).should('contain', 'Dashboard'); // Corrigido seletor com "." antes da classe
   });
 
   it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get(elementsList.professionalField).should('be.visible').type('Admin');
-    cy.get("[name='password']").should('be.visible').type('admin1234');
-    cy.get("[type='submit']").should('be.visible').click();
-    cy.get("[role='alert']").should('be.visible');  // Adicionado verificação de visibilidade
+    cy.get(selectorsList.professionalField).should('be.visible').type('Admin'); // Corrigido referência para selectorsList
+    cy.get(selectorsList.passwordField).should('be.visible').type('admin1234');
+    cy.get(selectorsList.loginButton).should('be.visible').click();
+    cy.get(selectorsList.wrongCredentialAlert).should('be.visible');  //
+    //  Adicionado verificação de visibilidade
   });
 
 });
+

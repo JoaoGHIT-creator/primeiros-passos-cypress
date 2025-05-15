9815381a5ccb1e59d0ede72e07d0547ec426e9d0
João Alves <joaooalves74@gmail.com>
2025-05-15 16:20:30 -0300

    Adding user data json

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,30 +1,43 @@
+import userData from '../../fixtures/user.json';
+
+
+
 describe('Orange HRM tests', () => {
-const selectorsList = {
-usernameField: "[name='nickname']",
-professionalField: "[name='username']",
-passwordField: "[name='password']",
-loginButton: "[type='submit']",
-sectionTitletopbar: ".oxd-topbar-header-breadcrumb-module",
-wrongCredentialAlert: "[role='alert']",
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    wrongCredentialAlert: "[role='alert']",
+    dashboardGrid: ".oxd-layout-navigation"
+  };
+  
+const userData = {
+  usersuccess: {
+    username: 'Admin',
+    password: 'admin123'
+  },
+  userfail: {
+    username: 'Admin',
+    password: 'admin1234'
+  }
 };
 
-  it('login - success', () => {  // Corrigido 'sucess' para 'success'
+  it('login - success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get(selectorsList.professionalField).should('be.visible').type('Admin'); // Corrigido referência para selectorsList
-    cy.get(selectorsList.passwordField).should('be.visible').type('admin123');
+    cy.get(selectorsList.usernameField).should('be.visible').type(userData.usersuccess.username);
+    cy.get(selectorsList.passwordField).should('be.visible').type(userData.usersuccess.password);
     cy.get(selectorsList.loginButton).should('be.visible').click();
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index');
-    cy.get(selectorsList.sectionTitletopbar).should('contain', 'Dashboard'); // Corrigido seletor com "." antes da classe
+    cy.get(selectorsList.sectionTitleTopBar).should('contain', 'Dashboard'); 
   });
 
   it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get(selectorsList.professionalField).should('be.visible').type('Admin'); // Corrigido referência para selectorsList
-    cy.get(selectorsList.passwordField).should('be.visible').type('admin1234');
+    cy.get(selectorsList.usernameField).should('be.visible').type(userData.userfail.username);
+    cy.get(selectorsList.passwordField).should('be.visible').type(userData.userfail.password);
     cy.get(selectorsList.loginButton).should('be.visible').click();
-    cy.get(selectorsList.wrongCredentialAlert).should('be.visible');  //
-    //  Adicionado verificação de visibilidade
+    cy.get(selectorsList.wrongCredentialAlert).should('be.visible');
+    
   });
-
 });
-
